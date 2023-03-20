749f7b0d5051b5bd725d77dbff87305fef3f4c90
tech-nirav <21it1@svitvasad.ac.in>
2023-03-21 00:01:06 +0530

    Logic bottombar & started homepage

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,29 +1,63 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
-class BottomBar extends StatelessWidget {
-  const BottomBar({super.key});
+import 'homeScreen.dart';
+
+class BottomBar extends StatefulWidget {
+  @override
+  static final List<Widget> _widgetoptions = <Widget>[
+    const HomeScreen(),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile"),
+  ];
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedindex = 0;
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedindex = index;
+    });
+  }
+  // const BottomBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("TICKET"),
-      ),
-      body: const Center(
-        child: Text("My Body"),
-      ),
+      // appBar: AppBar(
+      //   title: const Text("TICKET"),
+      // ),
+      body: Center(child: BottomBar._widgetoptions[_selectedindex]),
       bottomNavigationBar: BottomNavigationBar(
+          onTap: _onItemTap,
+          currentIndex: _selectedindex,
           showSelectedLabels: true,
           showUnselectedLabels: false,
           selectedItemColor: Colors.blue,
           unselectedItemColor: Colors.black,
+          type: BottomNavigationBarType.fixed,
+          // why bottom??
           elevation: 10,
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
             BottomNavigationBarItem(
-                icon: Icon(Icons.airplane_ticket), label: "Ticket"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+                icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+                label: "Ticket"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+                label: "Profile"),
           ]),
     );
   }
---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFeeedf2),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("Good morning"),
+                        Text("Book Tickets"),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              image: AssetImage("assets/images/img_1.png"))),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
